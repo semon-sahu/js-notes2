e88abc419d48ecabe0269271648651307f467d8c
Semon Sahu <111222semonsahu@gmail.com>
2025-01-14 12:39:23 +0530

    destructure object

---
+++ january-14-2025/index.js
@@ -0,0 +1,17 @@
+// const name= "semon";
+// const age=24;
+// const city= "bhopal";
+// export { name, age, city };
+
+// alert("hello")
+
+
+// const name="varun";
+// const age="age";
+
+// const collage=()=>{
+//     return "I am in a LNCT collage ";
+// }
+
+// export default collage;
+// export {name,age};
